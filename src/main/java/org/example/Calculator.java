2706fdf22fe69076ab45925cee619d18df226df0
Lisa-1919 <lizasimerova438@gmail.com>
2023-06-19 00:13:31 +0300

    calculation including brackets

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,78 +2,72 @@ package org.example;
 
 import org.apache.commons.lang.StringUtils;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
     public static String calculate(String stringToCalculate, double exchangeRate) {
-        String result = "";
         stringToCalculate = preprocessInput(stringToCalculate);
         LinkedList<String> operations = new LinkedList<>();
-        LinkedList<String> numbers = new LinkedList<>();
-        String tempFunction = "";
-        String tempNumber = "";
-        stringToCalculate = preprocessInput(stringToCalculate);
+        LinkedList<Double> numbers = new LinkedList<>();
+        parse(stringToCalculate, numbers, operations);
+        while (operations.size() != 0) {
+            operate(numbers, operations.removeLast());
+        }
+        return String.valueOf(numbers.removeLast());
+    }
+
+    private static void operate(LinkedList<Double> numbers, String operator) {
+        double r = numbers.removeLast();
+        switch (operator) {
+            case "+" -> numbers.add(r + numbers.removeLast());
+            case "-" -> numbers.add(numbers.removeLast() - r);
+            case "toDollars" -> numbers.add(r / 60);
+            case "toRubles" -> numbers.add(r * 60);
+        }
+    }
+
+    private static void parse(String stringToCalculate, LinkedList<Double> numbers, LinkedList<String> operations) {
+        StringBuilder tempFunction = new StringBuilder();
+        StringBuilder tempNumber = new StringBuilder();
         for (int i = 0; i < stringToCalculate.length(); i++) {
             if (stringToCalculate.charAt(i) == 't') {
-                while (i<stringToCalculate.length() & !Pattern.matches("[0-9]", String.valueOf(stringToCalculate.charAt(i)))) {
-                    tempFunction += stringToCalculate.charAt(i);
+                while (i < stringToCalculate.length() & !Pattern.matches("[0-9(]", String.valueOf(stringToCalculate.charAt(i)))) {
+                    tempFunction.append(stringToCalculate.charAt(i));
                     i++;
                 }
                 i--;
-                operations.add(tempFunction);
-                tempFunction = "";
+                operations.add(tempFunction.toString());
+                tempFunction = new StringBuilder();
             }
             if (Pattern.matches("[0-9]", String.valueOf(stringToCalculate.charAt(i)))) {
-                while (i<stringToCalculate.length() && Pattern.matches("[0-9.]", String.valueOf(stringToCalculate.charAt(i)))) {
-                    tempNumber += stringToCalculate.charAt(i);
+                while (i < stringToCalculate.length() && Pattern.matches("[0-9.]", String.valueOf(stringToCalculate.charAt(i)))) {
+                    tempNumber.append(stringToCalculate.charAt(i));
                     i++;
                 }
                 i--;
-                numbers.add(tempNumber);
-                tempNumber = "";
+                numbers.add(Double.parseDouble(tempNumber.toString()));
+                tempNumber = new StringBuilder();
             }
-            if (stringToCalculate.charAt(i)=='+' || stringToCalculate.charAt(i) =='-') {
+            if (stringToCalculate.charAt(i) == '+' || stringToCalculate.charAt(i) == '-' || stringToCalculate.charAt(i) == '(') {
                 operations.add(String.valueOf(stringToCalculate.charAt(i)));
             }
-        }
-        double r = Double.parseDouble(numbers.removeLast());
-        while (numbers.size() + operations.size() != 0) {
-            switch (operations.removeLast()) {
-                case "+" -> {
-                    r += Double.parseDouble(numbers.removeLast());
-                }
-                case "-" -> {
-                    r = Double.parseDouble(numbers.removeLast()) - r;
-                }
-                case "toDollars" ->{
-                    r = r/exchangeRate;
-                }
-                case "toRubles" ->{
-                    r = r*exchangeRate;
-                }
+            if(stringToCalculate.charAt(i) == ')'){
+                while (!operations.getLast().equals("("))
+                    operate(numbers,operations.removeLast());
+                operations.removeLast();
             }
         }
-        result = String.valueOf(r);
-        return result;
     }
 
     public static boolean isCorrect(String input) {
-        String currencySymbol = "";
         String currencyFunction = "[ +-]*((toDollars|toRubles)\\()[a-zA-Z0-9., $+-]*\\)[ +-]*[a-zA-Z0-9 $+-]*";
-        String brackets = "[a-z]*\\([a-zA-z0-9., $]*\\)";
-        return Pattern.matches(brackets, input);
-//        return Pattern.matches("([+ -]*(toDollars\\(|toRubles\\()(((\\$\\d+)|[+_ a-zA-Z()]|(\\d+p))*\\)))*", input);
+        return Pattern.matches(currencyFunction, input);
     }
 
     public static String preprocessInput(String stringToCalculate) {
-        stringToCalculate = stringToCalculate.replace(",", ".");
-        stringToCalculate = stringToCalculate.replace(" ", "");
-        stringToCalculate = stringToCalculate.replace("(", "");
-        stringToCalculate = stringToCalculate.replace(")", "");
+        stringToCalculate = stringToCalculate.replaceAll(",", ".");
+        stringToCalculate = stringToCalculate.replaceAll(" ", "");
         return stringToCalculate;
     }
 
